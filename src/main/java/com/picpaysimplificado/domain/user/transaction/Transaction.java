90883b0af9563d3984f64a0434a17e4b4230cb98
Lucas Lima <contactall.lucaslima@gmail.com>
2024-04-16 16:17:45 -0300

    feat: create Transaction entity

---
+++ src/main/java/com/picpaysimplificado/domain/user/transaction/Transaction.java
@@ -0,0 +1,32 @@
+package com.picpaysimplificado.domain.user.transaction;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import org.apache.catalina.User;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table
+@Getter
+@Setter
+@AllArgsConstructor
+@EqualsAndHashCode(of="id")
+public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private BigDecimal amount;
+    @ManyToOne
+    @JoinColumn(name = "sender_id")
+    private User Sender;
+    @ManyToOne
+    @JoinColumn(name = "receiver_id")
+    private User receiver;
+    private LocalDateTime timestamp;
+}
